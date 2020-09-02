1702968c3ec98680b2a06a318d0b4da13090dfb3
Francesca Litterio <francesca@litterio.net>
2020-09-02 15:52:12 -0400

    UPDATE 1
    Adds a random quote to the results page. Eliminated the generate button once it is clicked.

--- index.js
+++ index.js
@@ -2,42 +2,75 @@
 
 const pexelApiKey = '563492ad6f917000010000011bdc2da76e6344cfa398e5038f23b7a4';
 const pexelUrl = "https://api.pexels.com/v1/search";
-function formatQueryParams(params){
+const quoteUrl = "https://quote-garden.herokuapp.com/api/v2/quotes/random";
+
+function formatQueryParams(params){ //for pexel api search
   const queryItems = Object.keys(params).map(key => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
   return queryItems.join('&');
 }
 
-function displayResults(responseJson){
+function displayImage(responseJson){ //display random image from pexel search
   console.log(responseJson);
   const random = Math.floor(Math.random() * Math.floor(14));
-  $('#results').empty();
-  $('#results').append(`<li><img src=${responseJson.photos[random].src.medium}></li>`);
+  $('#image').empty();
+  $('#image').append(`<img src=${responseJson.photos[random].src.medium}>`);
+
+  $(getQuote);
 }
 
-function getImage(query){
-    const params ={
+function getImage(query){ //retrieve image from json
+    $('#js-form').empty(); //empties the form
+    const params ={ //gets search term
       query: query,
     }
     const queryString=
-    formatQueryParams(params);
-    const url = pexelUrl + "?" + queryString;
-    console.log(url);
+    formatQueryParams(params); 
+    const pexelApiUrl = pexelUrl + "?" + queryString; //makes pexel api url
+    console.log(pexelApiUrl);
 
-    const options = {
+    const options = { //authorizes use of pexelapi with key
       headers: new Headers({
         "Authorization": pexelApiKey})
   };
-    fetch(url, options)
-        .then(response => response.json())
-        .then(responseJson => displayResults(responseJson));
+    fetch(pexelApiUrl, options) //gets json
+        .then(response => {
+          if(response.ok) {
+            return response.json();
+          }
+          throw new Error(response.statusText);
+        })
+        .then(responseJson => displayImage(responseJson))
+        .cath(err => {
+          $('#js-error-message').text(`Something went wrong: ${err.message}`);
+        });
 }
 
-function watchForm(){
+  function displayQuote(responseJson){ //display random quote
+    console.log(responseJson);
+    $('#quote').empty();
+    $('#quote').append(`<h2><em>"${responseJson.quote.quoteText}"</em></h2><p>By ${responseJson.quote.quoteAuthor}</p>`);
+
+  }
+  function getQuote(){ //fetch json from random quote
+    fetch(quoteUrl)
+      .then(response => {
+        if(response.ok) {
+          return response.json();
+        }
+        throw new Error (response.statusText);
+      })
+      .then(responseJson => displayQuote(responseJson))
+      .catch(err =>{
+        $('#js-error-message').text(`Something went wrong: ${err.message}`);
+      });
+  }
+
+function watchForm(){ //event listener for initial form
   $('#js-form').submit(event => {
     event.preventDefault();
     const phrase = $("#js-search").val();
-    getImage(phrase);
+    getImage(phrase); //calls getImage function with input
   })
 }
 
-$(watchForm);
+$(watchForm); //renders the form
